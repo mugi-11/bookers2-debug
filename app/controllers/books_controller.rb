48baa4631e6a6df964c0d9cbdc72a93f6dbd7103
EC2 Default User <ec2-user@ip-172-31-3-6.ec2.internal>
2021-11-05 05:56:50 +0000

    [finished]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,34 +1,34 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
-  before_action :correct_book, only: [:edit]
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(@book.user_id)
-    @book_new = Book.new
+    @new_book = Book.new
+    @user = @book.user
   end
 
   def index
     @books = Book.all
     @book = Book.new
-    @user = current_user
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book) 
+      redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
-      @user = current_user
       render 'index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user_id == current_user.id
+      render 'edit'
+    else
+      redirect_to books_path
+    end
   end
 
 
@@ -36,11 +36,8 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "You have updated book successfully."
-      redirect_to book_path(@book)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      @books = Book.all
-      @user = current_user
       render "edit"
     end
   end
@@ -55,13 +52,6 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
-  end
-
-  def correct_book
-    book = Book.find(params[:id])
-    if book.user != current_user
-      redirect_to books_path
-    end
   end
 
 end
