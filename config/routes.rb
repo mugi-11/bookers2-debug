83434879a135e5f1d57a111823e15aba57d7921f
EC2 Default User <ec2-user@ip-172-31-3-6.ec2.internal>
2021-11-08 06:03:01 +0000

    検索機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  get '/search', to: 'searches#search'
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
@@ -12,4 +13,6 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
+  
+  
 end
